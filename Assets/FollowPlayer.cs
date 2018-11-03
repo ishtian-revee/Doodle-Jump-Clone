df3cfffdb8096e8f8f0935dc2f8bd1dea3bd071d
ishtian revee <ishtian11@gmail.com>
2018-11-03 13:53:47 +0600

    camera foolow implemented

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    // configurable fields
+    public Transform player;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+	    if (player.position.y > transform.position.y)
+	    {
+            Vector3 newPosition = new Vector3(transform.position.x, player.position.y, transform.position.z);
+	        transform.position = newPosition;
+	    }
+	}
+}
