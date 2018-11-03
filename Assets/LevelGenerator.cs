76aaf1f0d95a356cd83a0b77635453fe7898f4ba
ishtian revee <ishtian11@gmail.com>
2018-11-03 15:46:37 +0600

    basic level generation implemented

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    // configurable fields
+    public GameObject platformPrefab;
+    public int numberOfPlatforms;
+    public float levelWidth;
+    public float minY;
+    public float maxY;
+
+	// Use this for initialization
+	void Start ()
+	{
+        Vector3 spawnPosition = new Vector3();
+
+	    for (int i = 0; i < numberOfPlatforms; i++)
+	    {
+	        spawnPosition.y += Random.Range(minY, maxY);
+	        spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+	        Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
